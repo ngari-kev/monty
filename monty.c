7965f3fc5cdab2d8ed638762a4b61b16aeeb23e7
Nyamisachela <nyamisachelagat@gmail.com>
2023-11-16 04:10:19 -0800

    .c files

---
+++ monty.c
@@ -0,0 +1,47 @@
+#include "monty.h"
+/**
+*main - entry point
+*@argc: argument count
+*@argv: array of arguments
+*Return: 0(Success)
+*/
+int main(argc, **argv)
+{
+	FILE *file;
+	char buffer[LINE_MAX];/* Max that a line can hold */
+	int line_no;
+	stack_t *stack;
+
+	file = NULL;
+	stack = NULL;
+
+	/* array of instructions */
+	instruction_t instructions[] = {
+        {"push", push},
+        {"pop", pop},
+        {"pall", pall},
+        {NULL, NULL}/* array has to be null terminated */
+	}
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: monty <file.m>\n");
+		return (EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
+	while (fgets(buffer, sizeof(buffer), file) != NULL)
+	{
+		line_no++;
+		/* tokennize and invoke appropriate function */
+		processor(buffer, line_no, &stack, instructions);
+		/* clear buffer for next line */
+		memset(buffer, 0, sizeof(buffer));
+	}
+	/* close file */
+	fclose(file);
+	return (EXIT_SUCCESS);
+}
