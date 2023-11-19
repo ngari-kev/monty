e006b13822ea7487c6ced6a8d43dadd8c310b5f0
ngari-kev <kevngariwangui@gmail.com>
2023-11-20 00:34:28 +0300

    swap

--- swap.c
+++ swap.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
  * swap - Swaps the top two elements of the stack.
  * @stack: Double pointer to the head of the stack.
@@ -7,16 +6,15 @@
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-    int temp;
-    stack_t *current = *stack;
+	int temp;
+	stack_t *current = *stack;
 
-    if (current == NULL || current->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    temp = current->n;
-    current->n = current->next->n;
-    current->next->n = temp;
+	if (current == NULL || current->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = current->n;
+	current->n = current->next->n;
+	current->next->n = temp;
 }
